b8b223712034e7b25e01025502ee6784d2d7e108
Ben <benjamin.casazza@gmail.com>
2022-01-07 23:52:02 -0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 # This script is for update for quick & easy install via:
 sudo apt-get update && sudo apt-get upgrade -y && DEBIAN_FRONTEND=noninteractive sudo apt-get install -y --no-install-recommends curl git vim wget zsh
+sudo apt install nodejs -y && nodejs -v
+sudo apt install npm -y && npm -v
 # This script is for docker for quick & easy install via:
 curl -sSL https://get.docker.com/ | sh && sudo  usermod -aG docker $USER &&  newgrp docker
