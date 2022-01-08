e8cb502368779f80ca3508d165a9a7f12610f1dd
Ben <benjamin.casazza@gmail.com>
2022-01-07 22:50:47 -0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # This script is for update for quick & easy install via:
-apt-get update && apt-get upgrade -y && DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends curl git vim wget zsh
+sudo apt-get update && sudo apt-get upgrade -y && DEBIAN_FRONTEND=noninteractive sudo apt-get install -y --no-install-recommends curl git vim wget zsh
 # This script is for docker for quick & easy install via:
-curl -sSL https://get.docker.com/ | sh &&   usermod -aG docker ubuntu &&  newgrp docker
+curl -sSL https://get.docker.com/ | sh && sudo  usermod -aG docker ubuntu &&  newgrp docker
