1b5b0eeea6550367db47d93743f49e680a3a473e
Ben <benjamin.casazza@gmail.com>
2022-01-07 22:29:12 -0600

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# This script is for update for quick & easy install via:
+apt-get update && apt-get upgrade -y && DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends curl git vim wget zsh 
+# This script is for docker for quick & easy install via:
+sh -c '$(curl -sSL https://get.docker.com/)' &&   usermod -aG docker ubuntu &&  newgrp docker 
